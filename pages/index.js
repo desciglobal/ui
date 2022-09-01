6b78930a0bef3ad7eb111fc9c85826637431adfa
Johannes Weniger <jo@Johanness-Air.fritz.box>
2022-09-01 16:25:27 +0200

    og tags for twittwer

--- pages/index.js
+++ pages/index.js
@@ -1,48 +1,63 @@
-import {getAllEvents} from '../event-data';
-import EventList from '../components/events/event-list';
-import EventListHeading from '../components/events/event-list-heading';
+import { getAllEvents } from "../event-data";
+import EventList from "../components/events/event-list";
+import EventListHeading from "../components/events/event-list-heading";
 
-import Hero from '../components/hero/hero';
+import Hero from "../components/hero/hero";
 import Head from "next/head";
 
-
 function HomePage() {
-  const {upcomingEventsAsc, pastEventsAsc} = getAllEvents();
+  const { upcomingEventsAsc, pastEventsAsc } = getAllEvents();
 
   return (
     <>
       <Head>
-
         <title>Desci events around the globe</title>
-        <meta name="description" content="A list of descentralized science events around the globe. Contribute and share Events" />
-        {/* Facebook Meta Tags */}
         <meta
-          property="og:url"
-          content="desci.global"
+          name="description"
+          content="A list of descentralized science events around the globe. Contribute and share Events"
         />
+        {/* Facebook Meta Tags */}
+        <meta property="og:url" content="desci.global" />
         <meta property="og:type" content="website" />
         <meta property="og:title" content="Desci events around the globe" />
-        <meta property="og:description" content="A list of descentralized science events around the globe. Contribute and share Events" />
+        <meta
+          property="og:description"
+          content="A list of descentralized science events around the globe. Contribute and share Events"
+        />
         <meta
           property="og:image"
           content="https://desciglobal.vercel.app/images/og-image.png"
         />
         {/* Twitter Meta Tags */}
-        <meta name="twitter:card" content="https://desciglobal.vercel.app/images/og-image.png" />
+        <meta name="twitter:card" content="summary_large_image" />
+        <meta property="twitter:domain" content="desci.global" />
+        <meta
+          property="twitter:url"
+          content="desci.global"
+        />
+        <meta name="twitter:title" content="Desci events around the globe" />
+        <meta
+          name="twitter:description"
+          content="A list of descentralized science events around the globe. Contribute and share Events"
+        />
+        <meta
+          name="twitter:image"
+          content="https://desciglobal.vercel.app/images/og-image.png"
+        />
       </Head>
 
-      <Hero/>
+      <Hero />
       <div>
         <EventListHeading
           EventCount={upcomingEventsAsc.length}
           text="Upcoming Events"
         />
         <ul>
-          <EventList items={upcomingEventsAsc}/>
+          <EventList items={upcomingEventsAsc} />
         </ul>
-        <EventListHeading text="Past Events"/>
+        <EventListHeading text="Past Events" />
         <ul>
-          <EventList items={pastEventsAsc}/>
+          <EventList items={pastEventsAsc} />
         </ul>
       </div>
     </>
