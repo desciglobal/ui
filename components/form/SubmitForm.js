e5681e34df60ff86fe64a4cac222e57b2c91df4d
Johannes Weniger <jo@Johanness-Air.Home>
2022-09-22 17:17:09 +0100

    updated css for textinput

--- components/form/SubmitForm.js
+++ components/form/SubmitForm.js
@@ -22,13 +22,13 @@ function SubmitForm() {
             console.log(data)
         })}>
 
-        <TextField className={classes.TextInput} id="outlined-basic" variant="outlined" {...register("title", {required: "Event Title is required"})} placeholder="Event Title" />
+        <TextField className={classes.textinput} id="outlined-basic" variant="outlined" {...register("title", {required: "Event Title is required"})} placeholder="Event Title" />
         <p>{errors.title?.message}</p>
-        <TextField className={classes.TextInput} type="date" id="outlined-basic"  variant="outlined" {...register("date", {required: "Date is required"})} placeholder="Event Date" />
+        <TextField className={classes.textinput} type="date" id="outlined-basic"  variant="outlined" {...register("date", {required: "Date is required"})} placeholder="Event Date" />
         <p>{errors.date?.message}</p>
-        <TextField className={classes.TextInput} id="outlined-basic" variant="outlined" {...register("link", {required: "Some Link is required"})} placeholder="Link to Event Page" />
+        <TextField className={classes.textinput} id="outlined-basic" variant="outlined" {...register("link", {required: "Some Link is required"})} placeholder="Link to Event Page" />
         <p>{errors.link?.message}</p>
-        <TextField className={classes.TextInput} id="outlined-basic" variant="outlined" {...register("image")} placeholder="Link to Image" />
+        <TextField className={classes.textinput} id="outlined-basic" variant="outlined" {...register("image")} placeholder="Link to Image" />
         <p></p>
         <button className={classes.btn} type="submit">Submit Form</button>
         
