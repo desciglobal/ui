46bb36f56676a04021992c4e91b35318808896e0
Konstantin Kornev <konstantin.kornev.1@gmail.com>
2023-02-13 10:19:39 -0600

    restyle /submit-event page (except date&time)

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,25 +7,29 @@ module.exports = {
   theme: {
     extend: {
       backgroundImage: {
-        'hero': "url('/images/hero-bg.svg')",
-        'featured': "url('/images/featured-bg.png')",
-        'footer': "url(/images/footer.png)",
-        'nav': 'url(/images/nav-bg.png)'
+        hero: "url('/images/hero-bg.svg')",
+        featured: "url('/images/featured-bg.png')",
+        footer: "url(/images/footer.png)",
+        nav: "url(/images/nav-bg.png)",
       },
       colors: {
-        transparent: 'transparent',
-        current: 'currentColor',
-        desciblue: '#0A22F5',
-        descired: '#FF1414',
-        descigrey: '#F5F5F5',
-        descigreyfont: '#464646',
+        transparent: "transparent",
+        current: "currentColor",
+        desciblue: "#0A22F5",
+        descired: "#FF1414",
+        descigrey: "#F5F5F5",
+        descigreyfont: "#464646",
       },
     },
   },
   plugins: [
     require("@tailwindcss/forms")({
-      strategy: 'class', // only generate classes
+      strategy: "class", // only generate classes
     }),
+    require("daisyui"),
     // ...
   ],
-}
+  daisyui: {
+    themes: ["light"],
+  },
+};
